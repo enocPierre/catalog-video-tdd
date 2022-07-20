f56e68300fe64d64c640d7646df8a555982b6a01
enocPierre <enoc.pierre@mercadolivre.com>
2022-07-20 20:03:27 -0300

    tdd update

--- codeedu/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
+++ codeedu/domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -62,6 +62,24 @@ public class Category  extends AggregateRoot<CategoryID> {
         return  this;
     }
 
+    public Category update(
+            final String aName,
+            final String aDescription,
+            final boolean isActive
+    ){
+        this.name = aName;
+        this.description = aDescription;
+        if (isActive) {
+            activate();
+        }else {
+            desactivate();
+        }
+        this.name = aName;
+        this.description = aDescription;
+        this.updatedAt = Instant.now();
+        return  this;
+    }
+
     public CategoryID getId() {
         return id;
     }
